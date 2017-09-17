2658a65193de9a26ee06ce97b460a047db793056
Manraaj Nijjar <ManraajNijjar@gmail.com>
2017-09-17 17:38:14 -0400

    Initial Commit

---
+++ EmojiTextValidator/EmojiTextValidator.h
@@ -0,0 +1,19 @@
+//
+//  EmojiTextValidator.h
+//  EmojiTextValidator
+//
+//  Created by Manraaj Nijjar on 9/17/17.
+//  Copyright © 2017 Manraaj Nijjar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EmojiTextValidator.
+FOUNDATION_EXPORT double EmojiTextValidatorVersionNumber;
+
+//! Project version string for EmojiTextValidator.
+FOUNDATION_EXPORT const unsigned char EmojiTextValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmojiTextValidator/PublicHeader.h>
+
+
